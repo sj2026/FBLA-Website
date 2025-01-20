94d13ed022c40a4c358554ce2f6f715e519b8113
darsh-rewri <darsh.rewri@gmail.com>
2025-01-20 13:43:56 -0600

    Update Resume.py

--- pages/Resume.py
+++ pages/Resume.py
@@ -1,15 +1,12 @@
 import dash
-from dash import  Input, Output, html, State, callback, dcc
+from dash import Input, Output, html, State, callback, dcc
 import dash_bootstrap_components as dbc
 from db.ResumeDataAccess import ResumeDataAccess
 from beans.Resume import Resume
 
-<<<<<<< HEAD
+
 dash.register_page(__name__, path_template="/resume/<mode>/<resume_id>")
-=======
-#app = Dash(external_stylesheets=[dbc.themes.BOOTSTRAP])
-dash.register_page(__name__, path_template = "/resume/<mode>/<resume_id>")
->>>>>>> 22d810a160b1d39e509d7ad6fbb0075682f55579
+
 
 # Default resume data structure
 resume = {
@@ -19,6 +16,7 @@ resume = {
     'skillset': 'Enter your skillsets',
     'summary': 'Enter a summary',
 }
+
 
 # Standard input style
 input_style = {
@@ -32,6 +30,7 @@ input_style = {
     "overflowY": "auto",
 }
 
+
 # Textarea style with scroll
 textarea_style = {
     "width": "100%",
@@ -44,6 +43,7 @@ textarea_style = {
     "overflowY": "auto",
     "border": "1px solid #1a1f61",
 }
+
 
 # Navbar styling
 navbar = html.Div(
@@ -84,17 +84,21 @@ navbar = html.Div(
     ],
 )
 
+
 # Layout function for Resume page
 def layout(mode=None, resume_id=None, **kwargs):
     global screenMode
     screenMode = mode
 
+
     global resumeID
     resumeID = resume_id
+
 
     global resume
     readOnly = ''
     onlyRead = False
+
 
     # Set resume details for 'view' and 'edit' modes
     if resume_id is None:
@@ -114,9 +118,11 @@ def layout(mode=None, resume_id=None, **kwargs):
         resume['skillset'] = resumeTemp.skillset
         resume['summary'] = resumeTemp.summary
 
+
     if mode == 'view':
         readOnly = "readOnly"
         onlyRead = True
+
 
     # Form inputs
     inputs = [
@@ -133,6 +139,7 @@ def layout(mode=None, resume_id=None, **kwargs):
             "placeholder": "Enter your Student ID",
         },
     ]
+
 
     textareas = [
         {
@@ -155,6 +162,7 @@ def layout(mode=None, resume_id=None, **kwargs):
         },
     ]
 
+
     input_rows = [
         dbc.Row(
             [
@@ -174,6 +182,7 @@ def layout(mode=None, resume_id=None, **kwargs):
         )
         for input_item in inputs
     ]
+
 
     textarea_rows = [
         dbc.Row(
@@ -195,6 +204,7 @@ def layout(mode=None, resume_id=None, **kwargs):
         for textarea_item in textareas
     ]
 
+
     # Submit button
     submitButton = html.Div(
         html.Button(
@@ -215,7 +225,9 @@ def layout(mode=None, resume_id=None, **kwargs):
         style={"textAlign": "center", "marginTop": "20px"},
     )
 
+
     form = dbc.Form(input_rows + textarea_rows + ([submitButton] if mode != "view" else []))
+
 
     return html.Div(
         style={
